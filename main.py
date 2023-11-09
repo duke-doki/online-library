9060846ecee6174270cef6f7365c6c33a274559b
duke-doki <145125297+duke-doki@users.noreply.github.com>
2023-11-09 20:21:12 +0300

    add code to the function

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ def download_txt(url, filename, folder):
     Path(f'{folder}').mkdir(exist_ok=True)
     response = requests.get(url)
     response.raise_for_status()
-    #check_for_redirect(response)
     filename = sanitize_filename(filename)
     with open(f'{folder}/{filename}.txt', 'w') as file:
         file.write(response.text)
@@ -28,12 +27,47 @@ def download_image(url, filename, folder):
     Path(f'{folder}').mkdir(exist_ok=True)
     response = requests.get(url)
     response.raise_for_status()
-    #check_for_redirect(response)
     filename = sanitize_filename(filename)
     with open(f'{folder}/{filename}', 'wb') as file:
         file.write(response.content)
 
     return os.path.join(folder, f'{filename}.txt')
+
+
+def parse_book_page(response):
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    title_tag = soup.find('h1')
+    book = title_tag.text.split('::')
+    title = f'{book_id}. {book[0].strip()}'
+    author = book[1].strip()
+
+    image = soup.find('div', class_='bookimage').find('img')['src']
+    image_url = urljoin('https://tululu.org/', image)
+    img_path = urlparse(image_url).path
+    img_name = img_path.split('/')[-1]
+
+    posts = soup.find_all(class_='texts')
+    all_comments = []
+    for post in posts:
+        comment = post.find('span')
+        all_comments.append(comment.text)
+
+    genres = soup.find('span', class_='d_book').find_all('a')
+    all_genres = []
+    for genre in genres:
+        all_genres.append(genre.text)
+
+    book_info = {
+        'title': title,
+        'author': author,
+        'image_url': image_url,
+        'img_name': img_name,
+        'comments': all_comments,
+        'genres': all_genres
+    }
+
+    return book_info
 
 
 for book_id in range(1, 11):
@@ -49,32 +83,16 @@ for book_id in range(1, 11):
     except requests.HTTPError:
         continue
     else:
-        soup = BeautifulSoup(response_book.text, 'lxml')
-
-        title_tag = soup.find('h1')
-        book = title_tag.text.split('::')
-        title = f'{book_id}. {book[0].strip()}'
-
-        image = soup.find('div', class_='bookimage').find('img')['src']
-        image_url = urljoin('https://tululu.org/', image)
-        img_path = urlparse(image_url).path
-        img_name = img_path.split('/')[-1]
-
         folder_for_books = 'books'
         folder_for_images = 'images'
 
-        posts = soup.find_all(class_='texts')
-        genres = soup.find('span', class_='d_book').find_all('a')
-        print('Заголовок', title)
+        book_info = parse_book_page(response_book)
 
-        for genre in genres:
-            print(genre.text)
-
-        print('Комментарии:')
-        for post in posts:
-            comment = post.find('span')
-            print(comment.text)
-
-        # download_txt(url_txt, title, folder_for_books)
-        # download_image(image_url, img_name, folder_for_images)
-
+        download_txt(url_txt,
+                     book_info['title'],
+                     folder_for_books
+                     )
+        download_image(book_info['image_url'],
+                       book_info['img_name'],
+                       folder_for_images
+                       )
