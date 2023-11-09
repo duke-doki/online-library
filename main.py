dcbd3372f94d79e4de0d2661ce87a8591f746670
duke-doki <145125297+duke-doki@users.noreply.github.com>
2023-11-09 14:12:14 +0300

    add check for redirect

--- main.py
+++ main.py
@@ -4,9 +4,20 @@ from pathlib import Path
 
 Path('books').mkdir(exist_ok=True)
 
-for book in range(10):
-    url = f'https://tululu.org/txt.php?id={32168 + book}'
+
+def check_for_redirect(response):
+    if response.history and response.url == 'https://tululu.org/':
+        raise requests.HTTPError
+
+
+for book_id in range(1, 11):
+    url = f'https://tululu.org/txt.php?id={book_id}'
     response = requests.get(url)
     response.raise_for_status()
-    with open(f'books/book{book}', 'w') as file:
-        file.write(response.text)
+    try:
+        check_for_redirect(response)
+    except requests.HTTPError:
+        continue
+    else:
+        with open(f'books/book{book_id}', 'w') as file:
+            file.write(response.text)
