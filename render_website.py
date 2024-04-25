f6c44b44f325b190b9b5cc83798d1a39eba914eb
duke-doki <145125297+duke-doki@users.noreply.github.com>
2024-04-25 18:55:15 +0300

    Split books list by pages

--- render_website.py
+++ render_website.py
@@ -1,4 +1,5 @@
 import json
+import os
 import pprint
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
@@ -9,22 +10,27 @@ def render_website():
     with open('downloaded_books.json', 'r') as file:
         books_json = file.read()
     books = json.loads(books_json)
-    columned_books = chunked(books, int(len(books)/2))
+    paged_books = chunked(books, 20)
 
-    env = Environment(
-        loader=FileSystemLoader('.'),
-        autoescape=select_autoescape(['html'])
-    )
-    template = env.get_template('index.html')
+    os.makedirs('pages', exist_ok=True)
+    for page_num, page in enumerate(paged_books):
+        columned_books = chunked(page, int(len(page) / 2))
+        env = Environment(
+            loader=FileSystemLoader('.'),
+            autoescape=select_autoescape(['html'])
+        )
+        template = env.get_template('index.html')
 
-    rendered_books = template.render(columns=columned_books)
+        rendered_books = template.render(columns=columned_books)
 
-    with open('rendered_index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_books)
+        page_path = os.path.join('pages', f'rendered_index_{page_num}.html')
+        with open(page_path, 'w', encoding="utf8") as file:
+            file.write(rendered_books)
 
 
 if __name__ == '__main__':
     server = Server()
     render_website()
     server.watch('index.html', render_website)
-    server.serve(root='.', default_filename='rendered_index.html')
+    start_page_path = os.path.join('pages', 'rendered_index_0.html')
+    server.serve(root='.', default_filename=start_page_path)
